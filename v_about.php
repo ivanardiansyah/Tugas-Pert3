89e1cee01310fca65bc47202d225d6c8553d6eca
ivanardiansyah <91322982+ivanardiansyah@users.noreply.github.com>
2021-09-30 21:37:01 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+ 	<h4>Nama</h4>
+ 	<ul type="disc">
+ 		<li>Nama Depan 		:Ivan Nur</li>
+ 		<li>Nama Belakang 	: Ardiansyah</li>
+ 	</ul>
+ <br>
+ 	<h4>Alamat</h4>
+ 	<ul type="none">
+ 		<li> Jl. Sabar Raya RT04/04 </li>
+ 	</ul>
+ 
+ 	<h4>Tempat Tanggal Lahir</h4>
+ 	<ul type="none">
+ 		<li>Jakarta, 18 04 2000</li>
+ 	</ul>
+ 	<h4>Sebuah Teori Konspirasi</h4>
+ 	<ul type="square">
+ 		<li>Apakah Bumi itu Bulat ??</li>
+ 		<li>Atau Semesta ini yang Persegi ??</li>
+ 	</ul>
+</section>
---
+++ v_footer.php
@@ -0,0 +1,6 @@
+		<footer>
+			<a href="http://www.Akuratdanpas.com">akurat.com</a>
+		</footer>
+	</div>
+</body>
+</html>
